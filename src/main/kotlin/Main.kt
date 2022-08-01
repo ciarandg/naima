516037a85adc83dc6008c9b99c07eb57f00ec338
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-01 13:05:55 -0600

    Create jda bot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import Environment.botToken
+import net.dv8tion.jda.api.JDABuilder
+
+val jda = JDABuilder
+    .createDefault(botToken)
+    .build()
+
 fun main(args: Array<String>) {
     println("Hello, fellow jazz heads!")
 }
