b0a72be181900464ed57aeebf4dc6b3465401418
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-01 12:30:22 -0600

    Add application entry point

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    application
 }
 
 group = "club.mileshighjazz"
@@ -21,4 +22,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    println("Hello, fellow jazz heads!")
+}
