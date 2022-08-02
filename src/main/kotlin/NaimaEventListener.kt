cdd167f1cf4b60c882dcb30f9fb9d5aa5eeb3edc
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-01 18:39:18 -0600

    Embed album cover for A Love Supreme in responses

--- src/main/kotlin/NaimaEventListener.kt
+++ src/main/kotlin/NaimaEventListener.kt
@@ -1,3 +1,6 @@
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.EmbedType
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -17,7 +20,13 @@ class NaimaEventListener : ListenerAdapter() {
             val requester = event.user.name
             val artist = event.getOption("artist")!!.asString
             val album = event.getOption("album")!!.asString
-            event.hook.sendMessage("$requester has requested `$album` by `$artist`").queue()
+            val coverEmbed = EmbedBuilder()
+                .setImage("https://coverartarchive.org/release-group/77cf47ba-58cd-3f3d-a5f9-79bf89860421/front")
+                .build()
+            event.hook
+                .sendMessage("$requester has requested `$album` by `$artist`")
+                .addEmbeds(coverEmbed)
+                .queue()
         }
     }
 }
