8fd278e494ef5ad6ec4944126b12a55f069beb8f
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-01 19:05:38 -0600

    Add searchForReleaseGroup method

--- src/main/kotlin/musicbrainz/MusicBrainz.kt
+++ src/main/kotlin/musicbrainz/MusicBrainz.kt
@@ -1,7 +1,26 @@
 package musicbrainz
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+
 object MusicBrainz {
-    fun searchForReleaseGroup(albumTitle: String, artistName: String) {
-        TODO()
+    private const val RELEASE_GROUP_ENDPOINT = "https://musicbrainz.org/ws/2/release-group"
+    private val client = HttpClient(CIO)
+
+    suspend fun searchForReleaseGroup(albumTitle: String, artistName: String): String {
+        val response = client.get(RELEASE_GROUP_ENDPOINT) {
+            accept(ContentType.Application.Json)
+            url {
+                parameters.append(
+                    "query",
+                    "\"$albumTitle\" AND artist:\"$artistName\""
+                )
+            }
+        }
+        return response.body()
     }
 }
