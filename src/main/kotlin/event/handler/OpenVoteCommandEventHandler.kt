0ae0b1a346ef35fca40979a18af6a618f552fed6
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-05 20:01:29 -0600

    Run linter

--- src/main/kotlin/event/handler/OpenVoteCommandEventHandler.kt
+++ src/main/kotlin/event/handler/OpenVoteCommandEventHandler.kt
@@ -21,7 +21,7 @@ class OpenVoteCommandEventHandler(event: SlashCommandInteractionEvent) : EventHa
             "No albums available for vote"
         } else {
             selection.mapIndexed { i, pick ->
-                "${i+1}. $pick"
+                "${i + 1}. $pick"
             }.reduce { acc, s ->
                 acc + "\n" + s
             }
