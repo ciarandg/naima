bc430f89598527c1eaa6f74983f04691cf185ab6
Ciaran De Groot <ciaran.deg@gmail.com>
2022-08-05 22:25:01 -0600

    Add empty close vote command handler

--- src/main/kotlin/event/handler/CloseVoteCommandEventHandler.kt
+++ src/main/kotlin/event/handler/CloseVoteCommandEventHandler.kt
@@ -1,4 +1,9 @@
 package event.handler
 
-class CloseVoteCommandEventHandler {
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+
+class CloseVoteCommandEventHandler(event: SlashCommandInteractionEvent) : EventHandler(event) {
+    override fun handle() {
+        event.deferReply().queue()
+    }
 }
