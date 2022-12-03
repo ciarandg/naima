3cca4690eec07ad0b7923d7053b307aa2ddc74c9
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-03 13:46:34 -0700

    Implement image generation

--- src/main/kotlin/discord/embed/MultiAlbumCoverEmbed.kt
+++ src/main/kotlin/discord/embed/MultiAlbumCoverEmbed.kt
@@ -6,6 +6,26 @@ import net.dv8tion.jda.api.entities.MessageEmbed
 import java.io.File
 
 class MultiAlbumCoverEmbed(releaseGroups: List<ReleaseGroup>) {
+    private val outPath = run {
+        val ids = releaseGroups.map { it.id }
+        val combined = ids.reduce { acc, id -> "$acc-$id" }
+        "/tmp/$combined.jpg"
+    }
+
+    private val imageMagickCommand = run {
+        val imageUrls = releaseGroups.map { AlbumCoverEmbed(it).imageUrl }
+        fun List<String>.spaceDelimit() = this.reduce { acc, s -> "$acc $s" }
+        val urlsCombined = imageUrls.map { '"' + it + '"' }.spaceDelimit()
+        listOf(
+            "magick",
+            urlsCombined,
+            "-resize $ALBUM_COVER_WIDTH_PX",
+            "-crop " + ALBUM_COVER_WIDTH_PX + "x" + ALBUM_COVER_WIDTH_PX,
+            "+append $outPath"
+        ).spaceDelimit()
+    }
+
+
     private val fallbackImageUrl = "https://coverartarchive.org/release-group/a5572828-f7f2-405d-8853-a2019b019e07/front"
     val imageUrl: String by lazy {
         "TODO"
@@ -13,12 +33,15 @@ class MultiAlbumCoverEmbed(releaseGroups: List<ReleaseGroup>) {
     fun build(): MessageEmbed = TODO()
 
     private fun generateImage(): File? {
-        val outPath = "/tmp/multi.jpg"
-        val processBuilder = ProcessBuilder("magick \"$fallbackImageUrl\" -resize 1000 $outPath")
+        val processBuilder = ProcessBuilder(imageMagickCommand)
         val process = processBuilder.start()
         process.waitFor()
         return File(outPath).let { if (it.isFile) it else null }
     }
 
     private fun uploadToObjectStorage() {}
+
+    companion object {
+        private const val ALBUM_COVER_WIDTH_PX = 500
+    }
 }
