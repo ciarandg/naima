2ec8516714cbcb2bec3be927c3c1a30c36c1a351
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-03 14:32:11 -0700

    Implement object storage upload

--- src/main/kotlin/discord/embed/MultiAlbumCoverEmbed.kt
+++ src/main/kotlin/discord/embed/MultiAlbumCoverEmbed.kt
@@ -1,16 +1,22 @@
 package discord.embed
 
+import io.minio.BucketExistsArgs
+import io.minio.MakeBucketArgs
+import io.minio.MinioClient
+import io.minio.UploadObjectArgs
 import musicbrainz.data.ReleaseGroup
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
 import java.io.File
 
 class MultiAlbumCoverEmbed(releaseGroups: List<ReleaseGroup>) {
-    private val outPath = run {
+    private val localFilename = run {
         val ids = releaseGroups.map { it.id }
         val combined = ids.reduce { acc, id -> "$acc-$id" }
-        "/tmp/$combined.jpg"
+        "$combined.jpg"
     }
+
+    private val localPath = "/tmp/$localFilename"
 
     private val imageMagickCommand = run {
         val imageUrls = releaseGroups.map { AlbumCoverEmbed(it).imageUrl }
@@ -21,7 +27,7 @@ class MultiAlbumCoverEmbed(releaseGroups: List<ReleaseGroup>) {
             urlsCombined,
             "-resize $ALBUM_COVER_WIDTH_PX",
             "-crop " + ALBUM_COVER_WIDTH_PX + "x" + ALBUM_COVER_WIDTH_PX,
-            "+append $outPath"
+            "+append $localPath"
         ).spaceDelimit()
     }
 
@@ -38,10 +44,27 @@ class MultiAlbumCoverEmbed(releaseGroups: List<ReleaseGroup>) {
         val processBuilder = ProcessBuilder(imageMagickCommand)
         val process = processBuilder.start()
         process.waitFor()
-        return File(outPath).let { if (it.isFile) it else null }
+        return File(localPath).let { if (it.isFile) it else null }
     }
 
-    private fun uploadToObjectStorage() {}
+    private fun uploadToObjectStorage() {
+        val bucketName = "naima"
+        val minioClient = MinioClient.builder()
+            .endpoint("https://play.min.io")
+            .credentials("Q3AM3UQ867SPQQA43P2F", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
+            .build()
+        val found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())
+        if (!found) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build())
+        }
+        minioClient.uploadObject(
+            UploadObjectArgs.builder()
+                .bucket(bucketName)
+                .`object`(localFilename)
+                .filename(localPath)
+                .build()
+        )
+    }
 
     companion object {
         private const val ALBUM_COVER_WIDTH_PX = 500
