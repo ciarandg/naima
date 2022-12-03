b0b33ce1fa7a6a29cb405e8b5a74a124ffceb036
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-02 20:31:56 -0700

    Use individual env vars for mongo user, pass, host

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -1,6 +1,9 @@
 object Environment {
     val botToken: String = System.getenv("BOT_TOKEN")
-    val mongoConnString: String = System.getenv("MONGODB_CONNSTRING")
+    val mongoUser: String = System.getenv("MONGODB_INITDB_ROOT_USERNAME")
+    val mongoPassword: String = System.getenv("MONGODB_INITDB_ROOT_PASSWORD")
+    val mongoHost: String = System.getenv("MONGODB_HOST")
+    val mongoConnString: String = "mongodb://${mongoUser}:${mongoPassword}@${mongoHost}"
 
     object ChannelIDs {
         val suggestionsChannel: String = System.getenv("SUGGESTIONS_CHANNEL_ID")
