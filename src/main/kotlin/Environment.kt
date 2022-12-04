1f1660f0fdec81f15136a2cdca949ae0d6cc158f
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-04 13:27:26 -0700

    Move mongo credentials into their own object

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -1,9 +1,12 @@
 object Environment {
     val botToken: String = System.getenv("BOT_TOKEN")
-    val mongoUser: String = System.getenv("MONGO_INITDB_ROOT_USERNAME")
-    val mongoPassword: String = System.getenv("MONGO_INITDB_ROOT_PASSWORD")
-    val mongoHost: String = System.getenv("MONGO_HOST")
-    val mongoConnString: String = "mongodb://$mongoUser:$mongoPassword@$mongoHost"
+
+    object MongoCredentials {
+        val user: String = System.getenv("MONGO_INITDB_ROOT_USERNAME")
+        val password: String = System.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        val host: String = System.getenv("MONGO_HOST")
+        val connString: String = "mongodb://$user:$password@$host"
+    }
 
     object S3Credentials {
         val accessKey = System.getenv("S3_ACCESS_KEY")
