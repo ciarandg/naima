d179857fc85b788c3d893cd06b9cb8a9315c034c
Ciaran De Groot <ciaran.deg@gmail.com>
2022-12-04 13:45:50 -0700

    Add a test for building with missing cover art

--- src/test/kotlin/discord/embed/MultiAlbumCoverEmbedTest.kt
+++ src/test/kotlin/discord/embed/MultiAlbumCoverEmbedTest.kt
@@ -4,9 +4,10 @@ import kotlinx.coroutines.runBlocking
 import musicbrainz.MusicBrainz.searchForReleaseGroup
 import musicbrainz.data.ReleaseGroup
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class MultiAlbumCoverEmbedTest {
-    private val validReleaseGroups: List<ReleaseGroup> = runBlocking {
+    private val validReleaseGroupsWithCoverArt: List<ReleaseGroup> = runBlocking {
         listOf(
             searchForReleaseGroup("London Calling", "The Clash"),
             searchForReleaseGroup("Doolittle", "Pixies"),
@@ -14,12 +15,27 @@ internal class MultiAlbumCoverEmbedTest {
         ).map { it!! }
     }
 
+    private val validReleaseGroupsMissingCoverArt: List<ReleaseGroup> = runBlocking {
+        listOf(
+            searchForReleaseGroup("Chet Baker Sings", "Chet Baker")
+        ).map { it!! }
+    }
+
     @Test
-    fun build() {
+    fun buildBestCase() {
         println("Building an embed with these release groups:")
-        println(validReleaseGroups)
-        val embed = MultiAlbumCoverEmbed(validReleaseGroups).build()
+        println(validReleaseGroupsWithCoverArt)
+        val embed = MultiAlbumCoverEmbed(validReleaseGroupsWithCoverArt).build()
         println("Resulting URL:")
         println(embed!!.image!!.url!!)
+    }
+
+    @Test
+    fun buildWithMissingCoverArt() {
+        val releaseGroups = validReleaseGroupsWithCoverArt.plus(validReleaseGroupsMissingCoverArt)
+        println("Building an embed with these release groups:")
+        println(releaseGroups)
+        val embed = MultiAlbumCoverEmbed(releaseGroups).build()
+        assertEquals(embed, null)
     }
 }
